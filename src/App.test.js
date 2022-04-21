d95bf75ccbad2749b209de67cffd8f9c53ad93c6
Marina <marina-l-p@mail.ru>
2022-04-21 17:43:26 +1000

    updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders mem battle app', () => {
+test('renders memes battle app', () => {
   render(<App />);
   const titleElement = screen.getByText(/Mem battle/i);
   expect(titleElement).toBeInTheDocument();
