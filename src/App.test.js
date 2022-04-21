8ee95659c690b475996836140c0f9f505a99380d
Marina <lutientinuviel99@gmail.com>
2022-04-21 17:32:33 +1000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders mem battle app', () => {
   render(<App />);
   const titleElement = screen.getByText(/Mem battle/i);
   expect(titleElement).toBeInTheDocument();
